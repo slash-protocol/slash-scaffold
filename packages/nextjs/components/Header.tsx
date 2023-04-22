ca80df00ae9783d9978bb24bfd701036a9b6ff01
Slash Protocol <81094497+slash-protocol@users.noreply.github.com>
2023-04-22 17:04:58 +0100

    H1 title & Logo Title name

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -80,10 +80,10 @@ export const Header = () => {
         </div>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6">
           <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
+            <Image alt="SE2 logo" className="cursor-pointer" fill src="/assets/slash-logo.png" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-eth</span>
+            <span className="font-bold leading-tight">Slash Protocol</span>
             <span className="text-xs">Ethereum dev stack</span>
           </div>
         </Link>
