6f14942959347f83a9a3f9675fd422c255db813e
Slash Protocol <81094497+slash-protocol@users.noreply.github.com>
2023-05-13 18:08:33 +0100

    Vault cards

--- packages/nextjs/pages/Vaults.tsx
+++ packages/nextjs/pages/Vaults.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import type { NextPage } from "next";
+import { VaultCard } from "~~/components/bonds/VaultCard";
 import { ContractData } from "~~/components/vaults/ContractData";
-import { ContractInteraction } from "~~/components/vaults/ContractInteraction";
 
 const Vaults: NextPage = () => {
   return (
@@ -14,7 +14,23 @@ const Vaults: NextPage = () => {
         <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
       </Head>
       <div className="grid lg:grid-cols-2 flex-grow" data-theme="vaults">
-        <ContractInteraction />
+        <div>
+          <VaultCard
+            title="./ZeroSlash"
+            description="Stake and lock up SLASH for 7 days to be fully protected from debase"
+            duration="7 days"
+          />
+          <VaultCard
+            title="http://DoubleSlash"
+            description="Provide WETH-SLASH LP to protect from debase by earning yield on staking"
+            duration="24 hours"
+          />
+          <VaultCard
+            title="Stable Slash"
+            description="Provide USDT-SLASH LP to protect from debase by earning yield on staking"
+            duration="24 hours"
+          />
+        </div>
         <ContractData />
       </div>
     </>
